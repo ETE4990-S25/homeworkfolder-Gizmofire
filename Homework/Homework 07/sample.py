1e45a19521438f34d09636eae3e427aecd528f94
Gordon <ktgordonlau@gmail.com>
2025-03-11 15:58:34 -0700

    update hw 7

---
+++ Homework/Homework 07/sample.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import mplcursors
+import numpy as np
+
+# Sample data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Create the plot
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+# Add interactive cursors
+mplcursors.cursor(ax, hover=True)
+
+# Add title and labels
+plt.title('Interactive Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+# Display the plot
+plt.show()

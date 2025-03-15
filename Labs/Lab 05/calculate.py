7aa3d43dfb9e2cd7b5a66c72a3331c327377c46f
Gordon <ktgordonlau@gmail.com>
2025-03-14 19:30:02 -0700

    added more test cases for lab 5

--- Labs/Lab 05/calculate.py
+++ Labs/Lab 05/calculate.py
@@ -2,6 +2,17 @@ class Calculator(object):
     def add(self,x,y):
         return x + y
     
+    def subtract(self,x,y):
+        return x - y
+    
+    def multiply(self,x,y):
+        return x * y
+    
+    def divide(self,x,y):
+        if y == 0:
+            raise ValueError("Can not divide by zero!")
+        return x / y
+    
 if __name__ == '__main__':
     calc = Calculator()
     result = calc.add(2,2)
--- Labs/Lab 05/calculate_test.py
+++ Labs/Lab 05/calculate_test.py
@@ -11,5 +11,26 @@ class TestCalculate(unittest.TestCase):
     def test_add_method_raises_typeerror(self):
         self.assertRaises(TypeError,self.calc.add, {1}, 2) 
 
+    def test_subtract_method_returns_correct_result(self):
+        self.assertEqual(2, self.calc.subtract(4,2))
+
+    def test_subtract_method_raises_typeerror(self):
+        self.assertRaises(TypeError, self.calc.subtract, {1}, 2)
+
+    def test_multiply_method_returns_correct_result(self):  
+        self.assertEqual(6, self.calc.multiply(2,3))
+
+    def test_multiply_method_raises_typeerror(self):
+        self.assertRaises(TypeError, self.calc.multiply, {1}, 2)
+
+    def test_divide_method_returns_correct_result(self):
+        self.assertEqual(2, self.calc.divide(6,3))
+
+    def test_divide_method_raises_typeerror(self):
+        self.assertRaises(TypeError, self.calc.divide, {1}, 2)
+
+    def test_divide_method_raises_valueerror(self):
+        self.assertRaises(ValueError, self.calc.divide, 2, 0)
+
 if __name__=='__main__':
     unittest.main()

4f739645e86007fc34af1203a906c22ded6a8c1c
Gordon <gordon@inseat.com>
2025-04-27 13:45:50 -0700

    lab 9  still hangs but is able to do up to 50%

--- Labs/Lab 09 - Log Aggregator/lab9Main.py
+++ Labs/Lab 09 - Log Aggregator/lab9Main.py
@@ -1,18 +1,35 @@
 import lab9_generator
 import lab9_parser
 import threading
+import sys
 
+
+
+dictionary = {"INFO": 0, "WARNING": 0, "ERROR": 0, "CRITICAL": 0}
 
 def main():
     t1 = threading.Thread(target=lab9_generator.logGenerator)
-    t2 = threading.Thread(target=lab9_parser.logParser, args=("logGLfolder\\log.log",))
+    t2 = threading.Thread(target=lab9_parser.logParser, args=("logGLfolder\\log.log",dictionary))
+    
+    
     t2.start()
     t1.start()
 
+    # timout added since program was being hung 
+    t2.join(timeout=10)
+    
+   
     t1.join()
-    t2.join()
+
+    
 
     print("Done!")
+    print("Final dictionary:", dictionary)
+    print("Exiting in 10 seconds...")
+
+    lab9_parser.exitTimer()
+
+   
     
 
 if __name__ == "__main__":
--- Labs/Lab 09 - Log Aggregator/lab9_parser.py
+++ Labs/Lab 09 - Log Aggregator/lab9_parser.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 
 
+
 dictionary = {"INFO": 0, "WARNING": 0, "ERROR": 0, "CRITICAL": 0}
 
 
@@ -16,7 +17,7 @@ def new_line_reader(log_file_path):
       while True:
         line = file_.readline()
         if not line:
-          time.sleep(0.1)  # Wait briefly if no new lines
+          time.sleep(0.001)  # Wait briefly if no new lines
           continue
         yield line
   except FileNotFoundError:
@@ -24,18 +25,17 @@ def new_line_reader(log_file_path):
   except Exception as e:
     print(f"An error occurred: {e}")
 
+# added this 
+def exitTimer():
+  # Exit timer for the program
+  print("Exiting in 10 seconds...")
+  time.sleep(10)
+  print("Exiting now.")
+  exit(0)
 
 
-# file_path = "logGL.log"
-# with open(file_path, "r") as file:
-#   for line in file:
-    
-#     print(line.split("|"))
-    
-#     dictionary.add(line.split("|"))
-# print(dictionary)
 
-def logParser(log_file_path):
+def logParser(log_file_path, data_dict):
 
   if not log_file_path:
     log_file_path = "logGL.log"
@@ -47,7 +47,13 @@ def logParser(log_file_path):
         print(f"New log entry: {line.strip()}")
         items = line.split("|")
         # format of data, [time, file_path, log_level, message]
-        print(items[3])
+        print(items[2])
+
+        if items[2].strip() in data_dict:
+            data_dict[items[2].strip()] += 1
+            print(f"Updated dictionary: {data_dict}")
+            
+
   except KeyboardInterrupt:
     print("\nMonitoring stopped.")
 
@@ -56,7 +62,8 @@ def logParser(log_file_path):
 
 if __name__ == "__main__":
   log_file_path = "logGL.log"
-  logParser(log_file_path)
+  logParser(log_file_path, dictionary)
+  exitTimer()
 
   
 

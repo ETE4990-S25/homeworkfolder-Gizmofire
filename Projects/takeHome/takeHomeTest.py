a5db8f479f186475f6acf36ee1cff47f7d954b33
Gordon <gordon@inseat.com>
2025-05-16 02:01:48 -0700

    checkpoinmt

--- Projects/takeHome/takeHomeTest.py
+++ Projects/takeHome/takeHomeTest.py
@@ -2,11 +2,12 @@ import math as m
 import requests
 import xmltodict
 import json
+import xml.etree.ElementTree as ET
 import random
 
 import threading
 import time
-
+import csv
 import os
 
 import logging
@@ -14,6 +15,9 @@ import logging
 
 
 
+
+# https://www.geeksforgeeks.org/xml-parsing-python/
+# really good info on parsing xml and saving to csv 
 
 rates = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", 
          "COP", "CZK", "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", 
@@ -31,6 +35,55 @@ ratesForBase = [r for r in rates if r != "USD" and r != "EUR" and r != "GBP"]
 
 
 # WRITE TO FILE WITH DATA
+
+def fetchDataFromXML(respose):
+    # create element tree object
+    tree = ET.parse(respose)
+
+    # get root element
+    root = tree.getroot()
+
+    # create empty list for news items
+    newsitems = []
+
+    # iterate news items
+    for item in root.findall('./channel'):
+        logger.info()
+        # empty news dictionary
+        news = {}
+        logger.info
+        print(item)
+        # iterate child elements of item
+        # for child in item:
+        #     news[child.tag] = child.text
+         
+                
+
+        # # append news dictionary to news items list
+        # newsitems.append(news)
+    
+    # return news items list
+    return newsitems
+
+
+def savetoCSV(newsitems):
+
+    # specifying the fields for csv file
+    fields = ['title', 'baseCurrency', 'targetCurrency', 'exchangeRate', 'inverseRate' ]
+
+    # writing to csv file
+
+    with open(f"./allDB.csv", 'w') as csvfile:
+
+        # creating a csv dict writer object
+        writer = csv.DictWriter(csvfile, fieldnames = fields)
+
+        # writing headers (field names)
+        writer.writeheader()
+
+        # writing data rows
+        writer.writerows(newsitems)
+
 
 def pullCurrInfo(base):
 
@@ -50,6 +103,7 @@ def pullCurrInfo(base):
 
     # Parse the XML data to a Python dictionary
     data_dict = xmltodict.parse(response.text)
+    savetoCSV(fetchDataFromXML(response.text))
     
 
 
@@ -57,7 +111,7 @@ def pullCurrInfo(base):
     json_data = json.dumps(data_dict, indent=4)
 
     # Print the JSON data
-    print(json_data)
+    print(json_data) 
 
     # Optionally, write the JSON data to a file
     with open(f"currInfo//{base}//{date}_exchange_rates_{base}.json", "w") as json_file:
@@ -83,6 +137,7 @@ if __name__ == "__main__":
     # for r in ratesForBase:
     #     pullCurrInfo(r)
 
+    os.remove("log//kingsLogs.log")
     logger = logging.getLogger("file_logger")
     logger.setLevel(logging.INFO)
 
@@ -95,10 +150,17 @@ if __name__ == "__main__":
 
     logger.warning("Starting Threads - GATHERING SCROLLS :)")
     threads = []
-    for r in ratesForBase:
-        thread = threading.Thread(target=task, args=(r,))
-        threads.append(thread)
-        thread.start()
+
+
+    r = random.choice(rates)
+    thread = threading.Thread(target=task, args=(r,))
+    threads.append(thread)
+    thread.start()
+
+    # for r in ratesForBase:
+    #     thread = threading.Thread(target=task, args=(r,))
+    #     threads.append(thread)
+    #     thread.start()
 
     for thread in threads:
         thread.join()
